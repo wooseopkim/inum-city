73f489c85c5e01b03feff4e6d765e0d446cb8fb0
Wooseop Kim <wooseop@protonmail.com>
2023-05-27 10:10:30 +0900

    Enforce project boundaries
    https://nx.dev/core-features/enforce-project-boundaries

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,6 +2,24 @@
 module.exports = {
 	extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
 	parser: '@typescript-eslint/parser',
-	plugins: ['@typescript-eslint'],
+	plugins: ['@typescript-eslint', '@nx'],
+	rules: {
+		'@nx/enforce-module-boundaries': [
+			'error',
+			{
+				allow: [],
+				depConstraints: [
+					{
+						sourceTag: 'scope:web',
+						onlyDependOnLibsWithTags: ['scope:shared', 'scope:web'],
+					},
+					{
+						sourceTag: 'scope:crawler',
+						onlyDependOnLibsWithTags: ['scope:shared', 'scope:crawler'],
+					},
+				],
+			},
+		],
+	},
 	root: true,
 };
