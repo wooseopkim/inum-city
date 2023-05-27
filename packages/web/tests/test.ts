8cc148b344cd5cb51d21c202ea487df0821465e9
Wooseop Kim <wooseop@protonmail.com>
2023-05-27 09:15:49 +0900

    Make Playwright test pass

--- packages/web/tests/test.ts
+++ packages/web/tests/test.ts
@@ -1,6 +1,6 @@
 import { expect, test } from '@playwright/test';
 
-test.skip('index page has expected h1', async ({ page }) => {
+test('placeholder test', async ({ page }) => {
 	await page.goto('/');
-	await expect(page.getByRole('heading', { name: 'Welcome to SvelteKit' })).toBeVisible();
+	await expect(page.getByRole('main')).toBeVisible();
 });
