badd90f6b9302afcf142c27de1156eefb4ae826a
Wooseop Kim <wooseop@protonmail.com>
2023-05-29 11:16:27 +0900

    Call search function when query is present

--- packages/web/src/lib/db/animals.ts
+++ packages/web/src/lib/db/animals.ts
@@ -3,26 +3,37 @@ import type { GenericSchema } from '@supabase/postgrest-js/dist/module/types';
 import type { AnimalRecord } from './AnimalRecord';
 import supabase from './client';
 
-type LoadParams = {
+type FilterBuilder = PostgrestFilterBuilder<GenericSchema, Record<string, unknown>, AnimalRecord[]>;
+
+type Paginator<T> = {
 	size: number;
-	after?: AnimalRecord;
+	after?: T;
 };
 
-export function loadAnimalPage({
-	size,
-	after,
-}: LoadParams): PostgrestFilterBuilder<GenericSchema, Record<string, unknown>, AnimalRecord[]> {
-	const unpaged = supabase
-		.from('animals')
-		.select<string, AnimalRecord>()
+type LoadParams = Paginator<AnimalRecord> & {
+	readonly pred?: string | ((builder: FilterBuilder) => FilterBuilder);
+};
+
+export function loadAnimalPage({ size, after, pred }: LoadParams): FilterBuilder {
+	const base = typeof pred === 'string'
+		? supabase.rpc('search_animals', { query: pred })
+		: supabase
+			.from('animals')
+			.select<string, AnimalRecord>();
+
+	const filtered = typeof pred === 'function'
+		? pred(base)
+		: base;
+
+	const sorted = filtered
 		.order('body->>happenDt', { ascending: false })
 		.order('body->>desertionNo', { ascending: false })
 		.limit(size);
 
 	if (after === undefined) {
-		return unpaged;
+		return sorted;
 	}
-	return unpaged
+	return sorted
 		.lte('body->>happenDt', after.body.happenDt)
 		.lt('body->>desertionNo', after.body.desertionNo);
 }
