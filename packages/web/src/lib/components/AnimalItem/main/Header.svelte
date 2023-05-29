d0588c2a8586ef7405d5087fd4975a80b8c54e7a
Wooseop Kim <wooseop@protonmail.com>
2023-05-29 11:47:32 +0900

    Make cards look better on smaller screens

--- packages/web/src/lib/components/AnimalItem/main/Header.svelte
+++ packages/web/src/lib/components/AnimalItem/main/Header.svelte
@@ -26,28 +26,55 @@
 			display: inline-block;
 			background-color: white;
 			border: var(--border-width) solid black;
-			--padding-horizontal: 0.5rem;
+			--padding-horizontal: 0.4vw;
 			--padding-vertical: 0.5rem;
 			padding-block-start: var(--padding-vertical);
 			padding-block-end: var(--padding-vertical);
 			padding-inline-start: var(--padding-horizontal);
 			padding-inline-end: var(--padding-horizontal);
 
+			@media (max-width: 768px) {
+				width: 100%;
+				text-align: center;
+			}
+
 			&:first-of-type {
 				border-start-start-radius: var(--card-border-radius);
 				border-end-start-radius: var(--card-border-radius);
 				padding-inline-start: calc(var(--padding-horizontal) * 2);
+
+				@media (max-width: 768px) {
+					border-end-start-radius: initial;
+					border-start-end-radius: var(--card-border-radius);
+					padding-inline-start: var(--padding-horizontal);
+				}
 			}
 			&:last-of-type {
 				border-end-end-radius: var(--card-border-radius);
 				border-start-end-radius: var(--card-border-radius);
 				padding-inline-end: calc(var(--padding-horizontal) * 2);
+
+				@media (max-width: 768px) {
+					border-start-end-radius: initial;
+					border-end-start-radius: var(--card-border-radius);
+					padding-inline-end: var(--padding-horizontal);
+				}
 			}
 			&:not(:first-of-type) {
-				border-left: none;
+				border-inline-start: none;
+
+				@media (max-width: 768px) {
+					border-inline-start: var(--border-width) solid black;
+					border-block-start: none;
+				}
 			}
 			&:not(:last-of-type) {
-				border-right: none;
+				border-inline-end: none;
+
+				@media (max-width: 768px) {
+					border-inline-end: var(--border-width) solid black;
+					border-block-end: none;
+				}
 			}
 			&::marker {
 				content: '';
